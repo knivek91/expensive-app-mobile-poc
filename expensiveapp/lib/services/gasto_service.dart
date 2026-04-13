ab6f671d215ac72fcbdb19fa7684e3da63fb842b
knviek91 <knivek91@gmail.com>
2026-04-12 18:04:38 -0600

    refactor: Improve GastoService stream handling and add disposal checks

--- expensiveapp/lib/services/gasto_service.dart
+++ expensiveapp/lib/services/gasto_service.dart
@@ -5,15 +5,24 @@ class GastoService {
   static GastoService? _instance;
 
   final List<Gasto> _gastos = [];
+  bool _disposed = false;
   late final StreamController<List<Gasto>> _gastosController =
-      StreamController<List<Gasto>>.broadcast(
-        onListen: () {
-          // Emit current state when a listener subscribes
-          _gastosController.add(List.unmodifiable(_gastos));
-        },
-      );
+      StreamController<List<Gasto>>.broadcast();
 
-  Stream<List<Gasto>> get gastosStream => _gastosController.stream;
+  Stream<List<Gasto>> get gastosStream => Stream.multi((controller) {
+    // First, emit current state immediately
+    controller.add(List.unmodifiable(_gastos));
+
+    // Then, forward events from the controller
+    final subscription = _gastosController.stream.listen(
+      controller.add,
+      onError: controller.addError,
+      onDone: controller.close,
+    );
+
+    // Cancel inner subscription when outer subscriber cancels
+    controller.onCancel = () => subscription.cancel();
+  });
 
   factory GastoService() {
     return instance;
@@ -27,6 +36,11 @@ class GastoService {
   }
 
   Future<void> agregarGasto(Gasto gasto) async {
+    if (_disposed) {
+      throw StateError(
+        'GastoService has been disposed and cannot accept new gastos',
+      );
+    }
     _gastos.add(gasto);
     _gastosController.add(List.unmodifiable(_gastos));
   }
@@ -49,6 +63,7 @@ class GastoService {
   }
 
   void dispose() {
+    _disposed = true;
     _gastosController.close();
     _instance = null; // Reset singleton so future calls create fresh service
   }
